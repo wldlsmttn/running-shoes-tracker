a30a8208953bcc2217433c5f749e9fde3927b4c1
wldlsmttn <64415937+wldlsmttn@users.noreply.github.com>
2025-01-24 13:14:34 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,13 @@ import ShoeComparator from '../components/ShoeComparator';
 
 export async function getStaticProps() {
   try {
-    const kv = await fetch(`https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${process.env.KV_SHOES_NAMESPACE}/values/shoes`, {
+    const kv = await fetch(`https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${process.env.KV_SHOES_NAMESPACE}/values/shoes_list`, {
       headers: {
         'Authorization': `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`
       }
     });
     
     const shoes = await kv.json() || [];
-
     return {
       props: {
         shoes,
@@ -31,7 +30,7 @@ export async function getStaticProps() {
 export default function Home({ shoes, lastUpdate }) {
   return (
     <div className="container mx-auto px-4 py-8">
-      <ShoeComparator initialData={shoes} lastUpdate={lastUpdate} />
+      <ShoeComparator initialData={Array.isArray(shoes) ? shoes : []} lastUpdate={lastUpdate} />
     </div>
   );
 }
