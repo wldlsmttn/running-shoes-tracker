39f2337d4b0616db7304286d65e90a3c4e64b0b2
wldlsmttn <64415937+wldlsmttn@users.noreply.github.com>
2025-01-24 10:27:07 +0100

    Create ShoeComparator.tsx

---
+++ src/components/ShoeComparator.tsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import _ from 'lodash';
+import { Search, SortAsc, SortDesc } from 'lucide-react';
+
+// Copier ici le contenu précédent du composant ShoeComparator
