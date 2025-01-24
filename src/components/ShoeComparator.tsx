1e3598bccd2e770509e1d1977d121eba96aacd41
wldlsmttn <64415937+wldlsmttn@users.noreply.github.com>
2025-01-24 11:15:38 +0100

    Update ShoeComparator.tsx

--- src/components/ShoeComparator.tsx
+++ src/components/ShoeComparator.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Card, CardHeader, CardTitle, CardContent } from '../ui/card';
 import _ from 'lodash';
 import { Search, SortAsc, SortDesc } from 'lucide-react';
 
